c0b03718ef3ef07b7270f2756f6d857bc75e1124
gabriel leibel <gfleibel@hotmail.com>
2023-08-11 13:36:25 -0300

    added photo delete button, image skeleton and loading icon

--- src/Components/Feed/FeedPhotosItem.jsx
+++ src/Components/Feed/FeedPhotosItem.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from './FeedPhotosItem.module.css';
+import Image from '../Helper/Image';
 
 const FeedPhotosItem = ({ photo, setModalPhoto }) => {
-
-  function handleClick(){
-    setModalPhoto(photo)
+  function handleClick() {
+    setModalPhoto(photo);
   }
   return (
     <li onClick={handleClick} className={styles.photo}>
-      <img src={photo.src} alt={photo.title} />
+      <Image src={photo.src} alt={photo.title} />
       <span className={styles.views}>{photo.acessos}</span>
     </li>
   );
--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -1,27 +1,41 @@
 import React from 'react';
 import styles from './PhotoContent.module.css';
 import { Link } from 'react-router-dom';
-import PhotoComments from './PhotoComments'
+import PhotoComments from './PhotoComments';
+import { UserContext } from '../../UserContext';
+import PhotoDelete from './PhotoDelete';
+import Image from '../Helper/Image';
 
 const PhotoContent = ({ data }) => {
   const { photo, comments } = data;
+  const user = React.useContext(UserContext);
+
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
-        <img src={photo.src} alt={photo.title} />
+        <Image src={photo.src} alt={photo.title} />
       </div>
       <div className={styles.details}>
         <div>
           <p className={styles.author}>
-            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            )}
+
             <span className={styles.views}>{photo.acessos}</span>
           </p>
-          <h1 className='title'>
+          <h1 className="title">
             <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
           </h1>
           <ul className={styles.attributes}>
             <li>{photo.peso} kg</li>
-            {photo.idade === 1 ? <li>{photo.idade} ano</li> : <li>{photo.idade} anos</li>}
+            {photo.idade === 1 ? (
+              <li>{photo.idade} ano</li>
+            ) : (
+              <li>{photo.idade} anos</li>
+            )}
           </ul>
         </div>
       </div>
